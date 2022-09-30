83ed9415649fd330692b29a1a208cc10e2db6d5c
Alex Arturi <alexandrea8@nycstudents.net>
2022-09-30 11:35:34 -0400

    committ

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class CircleRunner {
+public class CircleRunner {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(5.0);
+
+        circle1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        //I predict that it will print 25 * 3.14 which equals 78.5.
+
+        // D. run the code and confirm your prediction! Were you correct?
+        // Yes.
+    }
 }
